593a5984198a44458d5fd6aaef22cb899e06b348
dh <zg9uagfv@gmail.com>
2018-01-23 09:47:10 +0800

    add new sources

--- ubuntu_tools_setup.sh
+++ ubuntu_tools_setup.sh
@@ -7,8 +7,7 @@ sudo apt-get remove onboard deja-dup
 
 #---baisc---#
 sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install git-core subversion build-essential cmake
-sudo apt-get install openssh-server  
+sudo apt-get install build-essential git-core subversion cmake automake autoconf openssh-server -y
 
 #---Zeal---#
 sudo add-apt-repository ppa:zeal-developers/ppa
@@ -24,3 +23,32 @@ sudo apt install albert
 sudo add-apt-repository ppa:webupd8team/java    
 sudo apt-get update    
 sudo apt-get install oracle-java8-installer
+
+#first
+sudo apt-get install unity-tweak-tool -y
+
+#flatabulous主题安装
+
+sudo add-apt-repository ppa:noobslab/themes
+sudo apt-get update
+sudo apt-get install flatabulous-theme -y
+
+#numix图标
+sudo add-apt-repository ppa:numix/ppa
+sudo apt-get update
+sudo apt-get install numix-gtk-theme numix-icon-theme-circle numix-wallpaper-* -y
+
+sudo apt-get install fonts-wqy-microhei
+
+sudo apt-get install shutter -y
+
+#根据日出日落时间(设定经纬度)自动调节电脑屏幕的亮度、色彩(色温)，保护眼睛
+sudo apt-get install redshift -y
+
+#docker
+sudo apt-get -y install apt-transport-https ca-certificates curl
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce
+
