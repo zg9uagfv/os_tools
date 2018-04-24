039255befdbd4a3845d4e16e1934dee9013f56d3
dh <zg9uagfv@gmail.com>
2018-04-24 14:01:32 +0800

    update

---
+++ opencv_3.4.0_build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+sudo apt-get install build-essential -y
+sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev -y
+
+sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev -y
+
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
+sudo apt-get install libxvidcore-dev libx264-dev -y
+
+sudo apt-get install libatlas-base-dev gfortran pylint -y
+
+
+sudo apt-get install python2.7-dev python3.5-dev -y
+
+
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D INSTALL_C_EXAMPLES=OFF \
+    -D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib-3.4.0/modules \
+    -D BUILD_EXAMPLES=ON ../opencv-3.4.0/ && make -j8 && sudo make install
